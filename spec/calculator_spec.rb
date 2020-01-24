a772c0d0d18421b3cccb0da942e0c5a3f49be90c
jolly-assembler-1925 <jnzhang622@gmail.com>
2020-01-24 04:53:06 +0000

    Automatically backed up by Learn

--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -1,6 +1,7 @@
 # Code your solution in calculator.rb
 
 describe "./calculator.rb" do
+  first_number = "Hello world!" 
 
   it "contains a local variable called first_number that is assigned to a number" do
     first_number = get_variable_from_file('./calculator.rb', "first_number")
